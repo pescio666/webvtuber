7a0db653f2644a6095eaebd6f7c99d2181552c28
pescio666 <163924392+pescio666@users.noreply.github.com>
2024-06-10 16:03:32 -0400

    Add files via upload

---
+++ Entrega1-main/js/main.js
@@ -0,0 +1,8 @@
+/*----Ejecucion del carrusel Materialize----*/ 
+document.addEventListener('DOMContentLoaded', () => {
+    const elementsCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementsCarousel, {
+        
+    })
+
+} );
