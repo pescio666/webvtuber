c8dde37dab4cfd7bc52ce84f9ebcebdd158bf3a0
SOAURORA <134520375+SOAURORA@users.noreply.github.com>
2024-07-16 22:50:20 -0400

    test

--- Entrega1-main/Django/Django/settings.py
+++ Entrega1-main/Django/Django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a7#^ii_ju7v(kz78e+rgm=7rw!hx_pfrp2l$1_8hu48ep!=p_y'
+SECRET_KEY = 'django-insecure-a7#^ii_ju7v(kz78e+rgm=7rw!h_pfrp2l$1_8hu48ep!=p_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
